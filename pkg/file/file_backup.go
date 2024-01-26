857b0e0452708fcb5645ace7334d8c3620924dda
Robert Jefe Lindstädt <robert.lindstaedt@gmail.com>
2024-01-26 22:05:50 +0100

    feat(backup): recursively create backup directory
    Handles simple cases, such as the documented one, that easily runs into ENODIR:
    
    hostctl backup --path $HOME/hostctl/
    
    ...when you run that for the first time.

--- pkg/file/file_backup.go
+++ pkg/file/file_backup.go
@@ -3,15 +3,25 @@ package file
 import (
 	"fmt"
 	"io"
+	"os"
 	"path"
 	"time"
 )
 
-// Backup creates a copy of your hosts file to a new location with the date as extension.
+// Backup creates a copy of your hosts file to a new location with the date as
+// extension. It recursively creates the target directory if it does not exist.
 func (f *File) Backup(dst string) (string, error) {
 	_, _ = f.src.Seek(0, io.SeekStart)
 	bkpFilename := fmt.Sprintf("%s.%s", f.src.Name(), time.Now().UTC().Format("20060102"))
 	bkpFilename = path.Join(dst, path.Base(bkpFilename))
+
+	// check if directory exists, else make it
+	if _, err := f.fs.Stat(dst); os.IsNotExist(err) {
+		err := f.fs.MkdirAll(dst, os.ModePerm)
+		if err != nil {
+			return "", err
+		}
+	}
 
 	b, err := f.fs.Create(bkpFilename)
 	if err != nil {
